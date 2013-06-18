ccc6f036672abb15c06b576305ca9809ff9c406c
Stefan Mahlitz <stefan.mahlitz@salzwerk.net>
2013-06-18 21:15:58 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BAT::Application.config.secret_token = '7e8744c6126db47417652bb4eda671b0ec26cedf5f30784ce0e9ec1d598f8282bd32f3ffad05332855112393e4215221d494d3f597beab3671f18c8fb34b2381'
