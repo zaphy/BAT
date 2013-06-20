b7a0376a5b5aa5b1461cbc3699ad4016af36ab3b
Stefan Mahlitz <stefan.mahlitz@salzwerk.net>
2013-06-20 15:25:44 +0200

    after rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130620131842) do
+ActiveRecord::Schema.define(:version => 20130620132405) do
 
   create_table "departments", :force => true do |t|
     t.string   "name"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(:version => 20130620131842) do
     t.integer  "referee_id"
     t.datetime "created_at",                   :null => false
     t.datetime "updated_at",                   :null => false
+    t.string   "email"
+    t.integer  "default_team_id"
+    t.integer  "deputy_leader_id"
+    t.integer  "licence_responsible_id"
+    t.integer  "training_coordinator_id"
   end
 
   create_table "teams", :force => true do |t|
